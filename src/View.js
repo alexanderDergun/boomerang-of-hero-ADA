bf02785be2ee33d28e14b24737ae7fc541d4b011
Anton Gorozhankin <obrv.igl@gmail.com>
2022-07-15 15:17:30 +0300

    Render 0.1

--- src/View.js
+++ src/View.js
@@ -1,18 +1,20 @@
 // Сделаем отдельный класс для отображения игры в консоли.
-const Game = require('./Game')
+const Game = require('./Game');
 
 class View {
-  constructor () {
+  constructor() {
+    // const game = new Game()
+    // this.game = new Game();
   }
-  render() {
+
+  render(arr) {
     const yourTeamName = 'Elbrus';
 
     // Тут всё рисуем.
-    // console.clear();
-    console.log(this.game.track.join(' '));
+    console.clear();
+    console.log(arr.join('  '));
     console.log('\n\n');
     console.log(`Created by "${yourTeamName}" with love`);
   }
-
 }
 module.exports = View;
